d4ddbdfb9373ac56ec75e541c263c91930cdbb21
StoyanMihaylov99 <stoqn.business@gmail.com>
2023-10-27 21:35:05 +0300

    Uploading beans

---
+++ src/main/java/com/example/softwareecommers/config/BeanConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.softwareecommers.config;
+
+import com.example.softwareecommers.beans.LoggedUser;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    @SessionScope
+    public LoggedUser loggedUser(){
+        return new LoggedUser();
+    }
+
+}
