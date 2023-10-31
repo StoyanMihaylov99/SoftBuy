ece083ea0315edb1a37b640648d9bdb25aa72ee0
StoyanMihaylov99 <stoqn.business@gmail.com>
2023-10-31 22:46:57 +0200

    Upload entities.

---
+++ src/main/java/com/example/softwareecommers/models/entities/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.example.softwareecommers.models.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid-string")
+    @GenericGenerator(name = "uuid-string",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    public BaseEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public BaseEntity setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+
+
+}
